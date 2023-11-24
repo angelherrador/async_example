13b8bb43b79290047a6d073795825cb666e827d8
Angel <angel@herradormartinez.es>
2023-11-24 21:45:56 +0100

    final de la práctica por Eduard

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,23 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            background: Colors.white),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,10 +40,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String label = "No data";
+  var httpfuture = http.get(Uri.parse("http://numbersapi.com/random/math"));
+  /*another way to declare
+          var httpfuture = http.get(Uri.http("numbersapi.com","/random/math"));
+          */
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.deepPurpleAccent.shade200,
+
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
@@ -73,29 +86,45 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(label),
+            Container(
+              margin: const EdgeInsets.all(50),
+              child: FutureBuilder(
+                future: httpfuture,
+                builder: ((context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data?.body ?? "No data",
+                        style: const TextStyle(color: Colors.white, fontSize: 25));
+                  } else if (snapshot.hasError) {
+                    return Text("Error: $Error");
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                })
+              ),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          label = "Requesting...";
-          setState(() {});
-          var future = http.get(Uri.parse("http://numbersapi.com/random/math"));
-          /*another way to declare
-          var future = http.get(Uri.http("numbersapi.com","/random/math"));
-          */
-          future.then((response) {
+          //label = "Requesting...";
+          //setState(() {});
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => widget));
+          /*future.then((response) {
             label = response.body;
             setState(() {});
           }).catchError((error) {
             label = "Error: ${error.toString()}";
             setState(() {});
-          });
+          });*/
         },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'Refresh',
+        child: const Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
